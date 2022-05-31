6c7ae63e5c8958fe8b2d7bdbb12e9e6ed7cc8b95
Sarthak Kalpasi <skalpasi@gmail.com>
2022-05-31 22:03:16 +0530

    Comment : Linked List on Tue May 31 10:03:16 PM IST 2022

---
+++ setup-git-v1.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+
+# installing git if not installed for specific distro's
+
+if ! location="$(type -p "git")" || [ -z "git" ]; then
+
+	echo "#################################################"
+	echo "installing git for this script to work"
+	echo "#################################################"
+
+	# check if pacman is installed
+	if which pacman > /dev/null; then
+
+		sudo pacman -S --noconfirm git
+
+	fi
+
+fi
+
+#setting up git
+#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
+
+git init
+git config --global user.name "Sarthak Kalpasi"
+git config --global user.email "skalpasi@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=18000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
