865dd8208d5b856783a79c7d3245881953fe1a77
Sarthak Kalpasi <skalpasi@gmail.com>
2022-06-04 12:12:14 +0530

    str.c

--- DSA/str.c
+++ DSA/str.c
@@ -1,11 +1,23 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+void reverse(char *pass) {
+  for (int i = 0; i < strlen(pass) / 2; i++) {
+    char temp = pass[i];
+    pass[i] = pass[strlen(pass) - i - 1];
+    pass[strlen(pass) - i - 1] = temp;
+  }
+}
 int main() {
-  char a[] = "sarthak abc bcd efg";
+  char a[] = "abcadcba";
+  char s[strlen(a)];
+  char d[strlen(a)];
+  memcpy(s, a, strlen(a));
+  strcpy(d, a);
   // printf("%lu\n", strlen(a));
   // char name[10];
   // printf("Enter your name: ");
-  // // scanf("%s", &name);
+  // scanf("%s", &name);
   // gets(name);
   // printf("Your name: %s", name);
   int len = 0;
@@ -13,5 +25,9 @@ int main() {
     len++;
   }
   printf("%d", len);
+  printf("\n--%s-- --%s-- --%s--\n", a, s, d);
+  reverse(a);
+  if (strcmp(a, d) == 0)
+    printf("palindrome");
   return 0;
 }
